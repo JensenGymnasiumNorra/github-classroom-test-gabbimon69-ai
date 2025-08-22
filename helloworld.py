b7d150f81967696f40bf829ef0d9284a0004da55
gabbimon69-ai <gabbimon69@gmail.com>
2025-08-22 09:45:41 +0200

    "done"

--- helloworld.py
+++ helloworld.py
@@ -1,3 +1,4 @@
-# Namn
-# Datum
-# Skriv ett program som skriver ut "Hello world" och lämna sedan in uppgiften
+# namn
+# datum
+# Skriv ett program som skriver ut "Hello world" och lämna sedan in uppgi
+print("HELLO WORLD")
